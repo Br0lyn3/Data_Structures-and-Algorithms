c62f4bcd6e6b8f9e164269b01ac19242a66b794f
Brly <wanyamabrolyne@outlook.com>
2026-03-18 20:05:19 +0300

    Add files via upload

---
+++ traversing.py
@@ -0,0 +1,10 @@
+#Built-in list (dynamic arrays)
+
+arr = [1, 2, 3, 4, 5]
+
+def traverse(arr):
+    for i in range (len(arr)):
+        print(arr[i])
+
+    for element in arr:
+        print(element)
